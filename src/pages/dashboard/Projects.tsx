d87507074134704414561e695be00aa9cdd72de9
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 11:05:50 +0300

    Update Projects.tsx

--- src/pages/dashboard/Projects.tsx
+++ src/pages/dashboard/Projects.tsx
@@ -21,8 +21,7 @@ import {
   Wand2,
   Check,
 } from "lucide-react";
-
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface FileItem {
   path: string;
