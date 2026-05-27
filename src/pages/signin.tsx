0c347f2ac3fb04d954b2a3c306d2cacfe5918fdc
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 11:02:53 +0300

    Update signin.tsx

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from "../components/ui/card";
 import { Badge } from "../components/ui/badge";
 import { ArrowLeft, Code2, ShieldCheck, AlertCircle, X } from "lucide-react";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function SignIn() {
   const [lang, setLang] = useState<"fr" | "en">("fr");
