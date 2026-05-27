482a4dd870552b3f96b052e8b5a57e440726e1dc
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 11:01:16 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getUsers() {
     const response = await fetch(`${API_URL}/api/users`);
