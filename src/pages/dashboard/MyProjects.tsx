bd40a5640b6fa5133526aa737727dc75db11b3ab
Mihaja Taratra <andriamtaratra5@gmail.com>
2026-05-27 11:05:28 +0300

    Update MyProjects.tsx

--- src/pages/dashboard/MyProjects.tsx
+++ src/pages/dashboard/MyProjects.tsx
@@ -16,7 +16,7 @@ import {
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface SavedProject {
   id: number;
